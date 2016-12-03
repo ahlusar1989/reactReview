0f5728c7cc1d109119e51d189506947244a5ebb6
SARAN AHLUWALIA <ahlusar.ahluwalia@gmail.com>
2016-12-03 17:20:52 -0500

    30  -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_,------,  0   -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_|   /\_/\  0   -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-^|__( ^ .^)      -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-  ""  ""
      30 passing (192ms)

---
+++ TicTacToeStateful/Field.js
@@ -0,0 +1,20 @@
+const React = require('react');
+
+class Field extends React.Component {
+  render () {
+    const { 
+    	player, 
+    	onClick
+    } = this.props;
+    return (
+      <button
+      onClick={onClick}
+      className="field"
+      disabled= {!!player}>
+      {player}
+      </button>
+    );
+  }
+}
+
+module.exports = Field;
